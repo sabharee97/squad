561b2c192cdbb15574e048f701a3301ded1f7c82
sabharee97 <43450530+sabharee97@users.noreply.github.com>
2023-07-22 12:46:18 +0200

    .

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Content from './Content';
+
+const App = () => {
+  return (
+    <div>
+      {/* Add any other components or layout elements here */}
+      <Content />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Content.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+const content = () => {
+    return (
+        <div className="content">
+            <p>This is sample paragraph....</p>
+        </div>
+    )
+}
+ 
+
+export default content
